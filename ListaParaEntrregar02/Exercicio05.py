df779520f4c9e168de4f5686d3f94da31a5e6469
jorgemarquesferreirajunior <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2023-04-06 09:42:34 -0300

    alterado nome da pasta de exercicios para entregar 02

--- ListaParaEntrregar02/Exercicio05.py
+++
@@ -1,8 +0,0 @@
-# Escreva um algoritmo para ler um número inteiro (considere que serão lidos apenas
-# valores positivos e inteiros) e escrever se é par ou ímpar
-print('=-'*10, 'PAR OU ÍMPAR', '=-'*10)
-a = int(input('Digite um número qualquer inteiro e positivo: '))
-if a % 2 == 0:
-    print(f'Bem, o número {a} é PAR')
-else:
-    print(f'Olha só, o numero {a} é ÍMPAR')
