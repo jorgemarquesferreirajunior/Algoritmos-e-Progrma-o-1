dc0296a13f180f367fcfa9c5d699c3455629f2e3
JorgeJr <jorgemarquesferreirajunior@gmail.com>
2023-08-07 20:12:19 -0300

    refect: finshed

--- 2023_02/Lista01/ex01.c
+++ 2023_02/Lista01/ex01.c
@@ -7,6 +7,12 @@ w = u + v;
 
 printf ("%d\n", w);
 printf("%c", w);
+/*
+Cada caractere possui uma correspondência numérica a partir da tabela ASC, assim quando criamos a char w 
+a partir de u e v, estamos na verdado gerando um novo caractere, que nesse caso, é um emogi feliz.
+No primeiro print vemos a correspondencia numérica(inteiro - tipo int) da variavel w,
+e no segundo print vemos o caractere da tabela ASC que corresponde ao valor inteiro presente na varialvel w 
 
+*/
 return 0;
 }
