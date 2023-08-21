8853c63b4bbbb48bcf44fe766f9522676d903137
JorgeJr <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2023-08-21 09:56:48 -0300

    Update ex_15_b_.c
    finished

--- 2023_02/Lista02/ex_15_b_.c
+++ 2023_02/Lista02/ex_15_b_.c
@@ -2,32 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 /*
-Sabe-se que um número da forma n3 é igual a soma de n ímpares consecutivos.
+Sabe-se que um nÃºmero da forma n3 Ã© igual a soma de n Ã­mpares consecutivos.
 Exemplo: 13= 1, 23= 3+5, 33= 7+9+11,  43= 13+15+17+19,... Dado m,
-determine os ímpares consecutivos cuja soma é igual a n3 para n assumindo valores de 1 a m.
+determine os Ã­mpares consecutivos cuja soma Ã© igual a n3 para n assumindo valores de 1 a m.
 */
 int main()
 {
 int numero;
 int somatorio = 0;
+int cubo = 0;
 int passo = 1;
 printf("Digite qualquer numero inteiro positivo: ");
 scanf("%d", &numero);
+
 for(int a = 1; a <= numero; a++)
 {
     somatorio = somatorio + a;
 }
 int impares[somatorio];
-printf("%d ao cubo vale %d\nOu entao:\n", numero, numero *numero *numero);
-for(int c = 1; c <= somatorio; c++)
+
+memset(impares, 0, somatorio);
+
+printf("%d ao cubo vale %d\nOu entao: ", numero, numero *numero *numero);
+for(int c = 0; c < somatorio; c++)
 {
     impares[c] = passo;
     passo = passo + 2;
 }
-for(int k = somatorio - numero; k <= somatorio; k++)
+
+for(int j = somatorio - numero; j <= somatorio - 1; j++)
 {
-    printf("%d ", impares[k]);
+    cubo = cubo + impares[j];
+    printf("%d",impares[j]);
+    j < somatorio - 1 ? printf(" + ") : printf(" = ");
 }
+printf("%d\n\n", cubo);
+
 system("pause");
 return 0;
 }
