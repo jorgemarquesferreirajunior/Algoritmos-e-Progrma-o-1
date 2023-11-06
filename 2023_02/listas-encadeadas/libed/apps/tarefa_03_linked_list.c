8152227092f369357a83f2ba1f9a14476c4b8169
JorgeJr <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2023-11-06 11:56:09 -0300

    inicio-atv-3

--- 2023_02/listas-encadeadas/libed/apps/tarefa_03_linked_list.c
+++ 2023_02/listas-encadeadas/libed/apps/tarefa_03_linked_list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "linked_list.h"
+#define MAX 5
 
 /*
 Faça uma função para remover os n últimos elementos de uma
@@ -10,86 +11,71 @@ Faça a função para todos os tipos de listas: estática e dinâmica
 */
 
 
-int main(int argc, char const *argv[]){
 
+int main(int argc, char const *argv[]){
+    int n_items = 2;
     // REFAZER TUDO A PARTIR DAQUI...
-    puts("\nconcatenando listas estaticas...");
-    ESList *eslist01 = create_ESList(5);
-    ESList *eslist02 = create_ESList(5);
-    for (int i = 0; i < 5; i++){
-        append_in_ESList(eslist01, i);
-        append_in_ESList(eslist02, i + 50);
+    puts("");
+    puts("Removendo os n primeiros elementos de uma lista estatica");
+    ESList *estatic_list = create_ESList(MAX);
+
+    for (int i = 1; i <= MAX; i++){
+        append_in_ESList(estatic_list, i);
     }
     puts("\nESLista01");
-    print_ESList(eslist01);
-    puts("\nESLista02");
-    print_ESList(eslist02);
-
-    ESList *eslist03 = concat_ESList(eslist01, eslist02);
-
-    puts("\nLista concatenada");
-    print_ESList(eslist03);
-
-    puts("\n***************************************************************************");
-    puts("\nconcatenando listas simplesmente encadeadas...\n");
-    SList *slista01 = create_SList();
-    SList *slista02 = create_SList();
-
-    for (int i = 1; i < 6; i++){
-        add_end_SList(slista01, i);
-        add_end_SList(slista02, i * 10);
-    }
-    puts("\nLista01");
-    print_SList(slista01);
-    puts("\nLista02");
-    print_SList(slista02);
-
-    SList *slista03 = create_SList();
-
-    slista03 = concat_SList(slista01, slista02);
-    puts("\nLista concatenada");
-    print_SList(slista03);
+    print_ESList(estatic_list);
     
+    printf("Removendo os %d primeiros elementos da lista...\n",n_items);
+    removen_ESList(estatic_list, n_items);
+
+    puts("\nESLista01");
+    print_ESList(estatic_list);
     puts("\n***************************************************************************");
-    puts("\nconcatenando listas duplamente encadeadas...\n");
+    puts("Removendo os n primeiros elementos de uma lista simplesmente encadeada...\n");
+    SList *singly_list = create_SList();
 
-    DList *dlist01 = create_DList();
-    DList *dlist02 = create_DList();
-
-    for (int i = 5; i < 25; i = i + 5){
-        add_end_DList(dlist01, i);
-        add_end_DList(dlist02, i * 10);
+    for (int i = 1; i <= MAX; i++){
+        add_end_SList(singly_list, i + 5);
     }
 
-    puts("\nLista01");
-    print_DList(dlist01);
-    puts("\nLista02");
-    print_DList(dlist02);
+    n_items = 4;
 
-    DList *dlist03 = concat_DList(dlist01, dlist02);
-
-    puts("\nLista concatenada");
-    print_DList(dlist03);
+    print_SList(singly_list);
+    printf("Removendo os %d primeiros elementos da lista...\n",n_items);
+    removen_SList(singly_list, n_items);
+    print_SList(singly_list);
 
     puts("\n***************************************************************************");
-    puts("\nconcatenando listas circulares...");
+    puts("Removendo os n primeiros elementos de uma lista duplamente encadeada...\n");
 
-    CList *clist01 = create_CList();
-    CList *clist02 = create_CList();
+    DList *doubly_list = create_DList();
 
-    for (int i = 3; i < 30; i = i + 3){
-        add_end_CList(clist01, i);
-        add_end_CList(clist02, i * 2);
+    for (int i = 1; i <= MAX + 5; i++){
+        add_end_DList(doubly_list, i + 15);
     }
 
-    puts("\nLista01");
-    print_CList(clist01);
-    puts("\nLista02");
-    print_CList(clist02);
+    n_items = 7;
 
-    CList *clist03 = concat_CList(clist01, clist02);
-    puts("\nLista concatenada");
-    print_CList(clist03);
+    print_DList(doubly_list);
+    printf("Removendo os %d primeiros elementos da lista...\n",n_items);
+    removen_DList(doubly_list, n_items);
+    print_DList(doubly_list);
+
+    puts("\n***************************************************************************");
+    puts("Removendo os n primeiros elementos de uma lista duplamente circular...\n");
+
+    CList *circ_list = create_CList();
+
+    for (int i = 1; i <= MAX + 10; i++){
+        add_end_CList(circ_list, i + 20);
+    }
+
+    n_items = 12;
+
+    print_CList(circ_list);
+    printf("Removendo os %d primeiros elementos da lista...\n",n_items);
+    removen_CList(circ_list, n_items);
+    print_CList(circ_list);
 
     return EXIT_SUCCESS;
 }
